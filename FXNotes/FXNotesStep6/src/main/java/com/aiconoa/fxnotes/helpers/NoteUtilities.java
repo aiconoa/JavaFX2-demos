7e2b301a32517023943fcabb37dd21364bec965f
Thomas Gros <thomas.gros@gmail.com>
2014-02-13 09:29:17 +0100

    Added FXNotes Step 6

---
+++ FXNotes/FXNotesStep6/src/main/java/com/aiconoa/fxnotes/helpers/NoteUtilities.java
@@ -0,0 +1,94 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright 2014 AICONOA.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package com.aiconoa.fxnotes.helpers;
+
+import de.svenjacobs.loremipsum.LoremIpsum;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import com.aiconoa.fxnotes.model.HTMLNote;
+import com.aiconoa.fxnotes.model.Note;
+import com.aiconoa.fxnotes.model.Notebook;
+import com.aiconoa.fxnotes.model.VideoNote;
+import java.util.UUID;
+
+/**
+ * Generates notebooks and notes
+ * @author Thomas
+ */
+public class NoteUtilities {
+    public static List<Notebook> generatedNotebooks;
+    
+    public static List<Notebook> generateNotebooks() {
+        List<Notebook> notebooks = new ArrayList<>();
+
+        LoremIpsum loremIpsum = new LoremIpsum();
+        Random rand = new Random();
+
+        for (int i = 0; i < rand.nextInt(10) + 2; i++) {
+            Notebook notebook = new Notebook("notebook " + i);
+            
+            for (int j = 0; j < rand.nextInt(30) + 2; j++) {
+                Note note = null;
+                if(j%3 == 0) {
+                    note = new VideoNote(notebook);
+                    note.setContent( "http://download.oracle.com/otndocs/javafx/JavaRap_ProRes_H264_768kbit_Widescreen.mp4");
+                } else {
+                   note = new HTMLNote(notebook);
+                   note.setContent(loremIpsum.getWords(rand.nextInt(2000) + 1));
+                }
+                note.setId(UUID.randomUUID().toString());
+                note.getUpdatedOn().setYear(rand.nextBoolean() ? 113 : 112);
+                note.setTitle("Note number " + i + " - " + j);
+            }
+            
+            notebooks.add(notebook);
+        }
+        NoteUtilities.generatedNotebooks = notebooks;
+        return notebooks;
+    }
+    
+    public static List<Note> findAllNotes() {
+        return findAllNotes(generatedNotebooks);
+    }
+    
+    public static List<Note> findAllNotes(List<Notebook> notebooks) {
+        List<Note> notes = new ArrayList<>();
+        for(Notebook notebook: notebooks) {
+            notes.addAll(notebook.getNotes());
+        }
+        
+        return notes;
+    }  
+    
+    public static Note findNodeById(String id) {
+         List<Note> notes = findAllNotes();
+         for(Note n : notes) {
+             if(n.getId().equals(id)) {
+                 return n;
+             }
+         }
+         return null;
+    }
+}
---
+++ FXNotes/FXNotesStep6/src/main/java/com/aiconoa/fxnotes/uicontrol/note/HTMLNoteEditor.java
@@ -0,0 +1,80 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright 2014 AICONOA.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package com.aiconoa.fxnotes.uicontrol.note;
+
+import com.aiconoa.fxnotes.model.HTMLNote;
+import com.aiconoa.fxnotes.model.Note;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.web.HTMLEditor;
+
+/**
+ * FXML Controller class
+ *
+ * @author Thomas
+ */
+public class HTMLNoteEditor extends NoteEditorBase {
+
+    @FXML
+    private HTMLEditor noteContentHTMLEditor;
+
+    public HTMLNoteEditor() {
+        super("HTMLNoteEditor.fxml");
+    }
+
+    @FXML
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        super.initialize(url, rb);
+        noteContentHTMLEditor.setDisable(note != null);
+    }
+
+    @Override
+    public void setNote(Note newNote) {
+        if (newNote != null
+                && !(newNote instanceof HTMLNote)) {
+            throw new IllegalArgumentException("Note must be an HTMLNote");
+        }
+
+        super.setNote(newNote);
+
+        if (newNote != null) {
+            noteContentHTMLEditor.setHtmlText(newNote.getContent().toString());
+            noteContentHTMLEditor.setDisable(false);
+        } else {
+            noteContentHTMLEditor.setHtmlText("");
+            noteContentHTMLEditor.setDisable(true);
+        }
+
+        noteContentHTMLEditor.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent t) {
+                note.setContent(noteContentHTMLEditor.getHtmlText());
+            }
+        });
+    }
+}
